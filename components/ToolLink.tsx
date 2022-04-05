1be23fa494a88ef0ac9975922c78d12caa139aa4
devpenzil <ajoalex012@gmail.com>
2022-04-05 21:51:19 +0530

    converted to typescript

--- components/ToolLink.tsx
+++ components/ToolLink.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
-function ToolLink({name, link, desc}) {
+interface props {
+    name:string,
+    link:string,
+    desc:string
+}
+function ToolLink({name, link, desc}:props) {
     return (
         <div className='py-4 pl-6 text-lg'>
             <a className='underline hover:text-green-500 dark:text-purple-400 font-semibold' href={link}>{name}</a> - <span className='text-gray-600 dark:text-gray-200'>{desc}</span>
