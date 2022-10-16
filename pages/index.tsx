7b2f9c7dc449e5cd01147b9cb62e532c2d4b9193
devpenzil <ajo.a@neoito.com>
2022-10-16 15:03:12 +0530

    next

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="text-9xl">hello</div>;
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
