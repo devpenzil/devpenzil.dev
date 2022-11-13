626721a026e6fc434b55ee8cd8e2814fd1f2d31a
devpenzil <ajoalex012@gmail.com>
2022-11-14 00:32:11 +0530

    home page

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React, { Children } from "react";
+import NavBar from "../Navbar/NavBar";
+import "./Layout.module.css";
+function Layout({ children }: any) {
+  return (
+    <div>
+      <div className="w-[400px] h-[400px] absolute rounded-full blur-3xl bg-gradient-to-r from-cyan-500 to-blue-500 blurone " />
+      <div className="w-[400px] h-[400px] absolute rounded-full blur-3xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bottom-0 right-0 blurtwo " />
+      <NavBar />
+      <div className="container mx-auto">{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ components/Navbar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="absolute w-full p-4 justify-between flex">
+      <div>hello</div>
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default NavBar;
