e4f2ce574c35c607a25ee66e128245635b0bf68f
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-09-16 20:24:31 +0530

    v6 initial

--- src/components/grids/Map.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Map() {
-  return (
-    <div className="shadow-lg p-4 border rounded-2xl h-full w-full bg-[url('/map.png')] bg-cover"></div>
-  )
-}
-
-export default Map
