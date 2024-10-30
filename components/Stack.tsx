4d1017c594afcffce5824bd4387b65715bd7a836
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-10-30 19:38:19 +0530

    stack

---
+++ components/Stack.tsx
@@ -0,0 +1,34 @@
+const Stack = () => {
+  return (
+    <div>
+      <div className="relative flex overflow-x-hidden bg-black text-white font-semibold">
+        <div className="py-12 animate-marquee whitespace-nowrap ">
+          <span className="text-4xl mx-4">React Native</span>
+          <span className="text-4xl mx-4">Kotlin</span>
+          <span className="text-4xl mx-4">Swift</span>
+          <span className="text-4xl mx-4">TypeScript</span>
+          <span className="text-4xl mx-4">Git</span>
+          <span className="text-4xl mx-4">React Native</span>
+          <span className="text-4xl mx-4">Kotlin</span>
+          <span className="text-4xl mx-4">Swift</span>
+          <span className="text-4xl mx-4">TypeScript</span>
+          <span className="text-4xl mx-4">Git</span>
+        </div>
+        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+          <span className="text-4xl mx-4">React Native</span>
+          <span className="text-4xl mx-4">Kotlin</span>
+          <span className="text-4xl mx-4">Swift</span>
+          <span className="text-4xl mx-4">TypeScript</span>
+          <span className="text-4xl mx-4">Git</span>
+          <span className="text-4xl mx-4">React Native</span>
+          <span className="text-4xl mx-4">Kotlin</span>
+          <span className="text-4xl mx-4">Swift</span>
+          <span className="text-4xl mx-4">TypeScript</span>
+          <span className="text-4xl mx-4">Git</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stack;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
       },
     },
   },
