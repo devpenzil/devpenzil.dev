91ebb776c4982ee9c7b96cfe837ce30ad58eeddd
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-11-19 00:50:15 +0530

    ui

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,30 +1,36 @@
 import { blogList } from "@/db/blogs";
+import Head from "next/head";
 import Link from "next/link";
 import React, { FC } from "react";
 
 const Blogs: FC = () => {
   return (
-    <div className="container lg:w-2/3 mx-auto py-20 text-lg font-semibold font-SourGummy">
-      <div>
-        <Link href={"/"}>
-          <div className="text-3xl font-semibold mb-8 flex items-center">
-            <img src="/icons/back.svg" alt="" className="w-8 h-8" /> Blogs
-          </div>
-        </Link>
-        {blogList.map((obj, index) => {
-          return (
-            <div key={index} className="my-6">
-              <a href={obj.url}>
-                <div>{obj.name}</div>
-                <div className="text-slate-300 text-xs">
-                  {obj.time} . {obj.date}
-                </div>
-              </a>
+    <>
+      <Head>
+        <title>Blogs | Ajo Alex | React Native Engineer</title>
+      </Head>
+      <div className="container lg:w-2/3 mx-auto py-20 text-lg font-semibold font-SourGummy">
+        <div>
+          <Link href={"/"}>
+            <div className="text-3xl font-semibold mb-8 flex items-center">
+              <img src="/icons/back.svg" alt="" className="w-8 h-8" /> Blogs
             </div>
-          );
-        })}
+          </Link>
+          {blogList.map((obj, index) => {
+            return (
+              <div key={index} className="my-6">
+                <a href={obj.url}>
+                  <div>{obj.name}</div>
+                  <div className="text-slate-300 text-xs">
+                    {obj.time} . {obj.date}
+                  </div>
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,18 +1,39 @@
+import { projectList } from "@/db/projects";
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 function Projects() {
   return (
-    <div className="container lg:w-2/3 mx-auto py-20 text-lg font-semibold font-SourGummy">
-      <div>
-        <Link href={"/"}>
-          <div className="text-3xl font-semibold mb-8 flex items-center">
-            <img src="/icons/back.svg" alt="" className="w-8 h-8" /> Projects
+    <>
+      <Head>
+        <title>Projects | Ajo Alex | React Native Engineer</title>
+      </Head>
+      <div className="container lg:w-2/3 mx-auto py-20 text-lg font-semibold font-SourGummy">
+        <div>
+          <Link href={"/"}>
+            <div className="text-3xl font-semibold mb-8 flex items-center">
+              <img src="/icons/back.svg" alt="" className="w-8 h-8" /> Projects
+            </div>
+          </Link>
+          <div>
+            {projectList.map((obj, index) => {
+              return (
+                <>
+                  <a href={obj.url} key={index} target="_blank">
+                    <div className="text-lg">{obj.name}</div>
+                    <div className="text-sm text-slate-300 font-semibold">
+                      {obj.desc}
+                    </div>
+                  </a>
+                  <div className="h-3" />
+                </>
+              );
+            })}
           </div>
-        </Link>
-        Update soon
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
