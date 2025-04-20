9cf0a0cc3b6fcef4757d32729a92e1d52f524ab2
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-04-20 19:44:50 +0530

    ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import NavBar from "@/components/NavBar";
-import SplashCursor from "@/reactBits/SplashCursor";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PagesTopLoader } from "nextjs-toploader/pages";
@@ -10,7 +9,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <PagesTopLoader />
       <NavBar />
       <Component {...pageProps} />
-      <SplashCursor />
     </>
   );
 }
